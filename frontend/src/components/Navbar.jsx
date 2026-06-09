44b2f6e8d9701327c94dc052543bd894787fe57f
Cristian Gil Gz <chipsentinel@gmail.com>
2026-06-09 15:54:03 +0200

    feat: añadir buscador base en navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,6 +34,13 @@ function Navbar({ vistaActual, cambiarVista }) {
           Juego
         </button>
       </div>
+
+      {/* buscador sin logica */}
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
     </nav>
   )
 }
